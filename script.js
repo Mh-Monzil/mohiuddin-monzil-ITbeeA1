68bfd10b1ae15a5827bf670cd105e1905be7a5e9
Mh-Monzil <monzil246@gmail.com>
2024-08-22 10:52:28 +0600

    add:description toggling added

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 const headingButton = document.getElementById("headingButton");
+const descriptionButton = document.getElementById("descriptionButton");
 const closeHeading = document.getElementById("closeHeading");
+const closeDescription = document.getElementById("closeDescription");
 const headingBox = document.getElementById("headingBox");
+const descriptionBox = document.getElementById("descriptionBox");
 
-headingButton.addEventListener("click", (e) => {
-    headingBox.classList.remove("hidden")
-    headingButton.classList.add("hidden")
-})
+// visible input box
+function handleVisible(visibleButton, box) {
+  visibleButton.addEventListener("click", () => {
+    box.classList.remove("hidden");
+    visibleButton.classList.add("hidden");
+  });
+}
 
-closeHeading.addEventListener("click", (e) => {
-    headingBox.classList.add("hidden")
-    headingButton.classList.remove("hidden")
-})
+// visible toggle button and hide input box
+function handleHidden(hiddenButton, box, visibleButton) {
+  hiddenButton.addEventListener("click", () => {
+    box.classList.add("hidden");
+    visibleButton.classList.remove("hidden");
+  });
+}
+
+// visible buttons 
+headingButton.addEventListener(
+  "click",
+  handleVisible(headingButton, headingBox)
+);
+descriptionButton.addEventListener(
+  "click",
+  handleVisible(descriptionButton, descriptionBox)
+);
+
+
+// hidden buttons 
+closeHeading.addEventListener("click", handleHidden(closeHeading, headingBox, headingButton));
+
+closeDescription.addEventListener("click", handleHidden(closeDescription, descriptionBox, descriptionButton));
+
